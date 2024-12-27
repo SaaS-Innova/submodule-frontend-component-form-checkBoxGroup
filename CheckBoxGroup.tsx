cfef3ac836f743503b2648f5d1ff97b65432e005
Nayan Gajera <nayan@saasinnova.com>
2024-12-27 14:58:35 +0530

    Improve the UI when using the 'items per row' property.

--- CheckBoxGroup.tsx
+++ CheckBoxGroup.tsx
@@ -32,12 +32,12 @@ export const CheckBoxGroup = (props: IFormProps) => {
       case IFormFieldType.TOP_LABEL:
         labelClassName = "";
         fieldClassName = "field p-fluid";
-        divClassName = "grid flex flex-column";
+        divClassName = "grid";
         break;
       default:
-        labelClassName = "col-12 mb-3 md:col-3 md:mb-0";
+        labelClassName = "col-12 mt-2 mb-3 md:col-3 md:mb-0";
         fieldClassName = "field grid";
-        divClassName = "col-12 mt-3 md:col-9 grid relative";
+        divClassName = "col-12 mt-2 md:col-9 grid relative";
         break;
     }
 
@@ -52,12 +52,14 @@ export const CheckBoxGroup = (props: IFormProps) => {
     </label>
   );
   const widthStyle = { width: `${100 / itemPerRow}%` };
+
   const onChange = (e: CheckboxChangeEvent, field: any) => {
     const selectedValue = e.checked
       ? [...selectedValues, e.value]
       : selectedValues.filter((value) => value !== e.value);
     field.onChange(selectedValue.map((value) => Number(value)));
   };
+
   return (
     <div className={fieldClassName}>
       {fieldType !== IFormFieldType.NO_LABEL && labelElement}
@@ -65,7 +67,7 @@ export const CheckBoxGroup = (props: IFormProps) => {
         {options &&
           options.map((option) => {
             return (
-              <div className="p-1" key={option.value} style={widthStyle}>
+              <div className="p-2" key={option.value} style={widthStyle}>
                 <Controller
                   name={attribute}
                   control={control}
